04d682a5795de8109e9fb462b95e2c9ee150fd48
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-10-01 09:54:09 -0500

    config allowed origins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+
+  const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',') || '*';
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 }
 bootstrap();
