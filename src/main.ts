9a211c0c63440e98aff5ce54bcb67316eec84eef
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-10-20 22:49:27 -0500

    enable cors all origins

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,9 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  const allowedOrigins = serverConfig.allowedOrigins;
-
   app.enableCors({
-    origin: allowedOrigins,
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
