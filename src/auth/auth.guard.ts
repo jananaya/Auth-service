b3c5707ac06224573e9f1df8058c145e5e128270
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-10-27 16:33:38 -0500

    add auth guard for token verification

---
+++ src/auth/auth.guard.ts
@@ -0,0 +1,46 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { JwtConfig } from 'src/config/interfaces/jwt-config.interface';
+import { jwtConfig } from 'src/config/jwt.config';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private readonly jwtConfig: JwtConfig;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {
+    this.jwtConfig = jwtConfig(this.configService);
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.jwtConfig.secret,
+      });
+
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
