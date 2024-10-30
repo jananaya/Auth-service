8b5963558e6ad6ff4772e4a3094c014627b4de0e
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-10-30 09:31:44 -0500

    add tags to the swagger document

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder().build();
+  const config = new DocumentBuilder()
+    .setTitle('Auth service API')
+    .setDescription('Role based authentication service')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
