6bfc874136511d86e8c5f12c7396e0abe38c3bca
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2024-11-06 17:39:43 -0500

    feat(users-module): role parameterization when creating a user

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
@@ -13,4 +13,9 @@ export class CreateUserDto {
   @ApiProperty()
   @IsNotEmpty()
   password: string;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsNotEmpty()
+  roleId: number;
 }
